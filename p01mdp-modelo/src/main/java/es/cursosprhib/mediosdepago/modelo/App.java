9f0a05584d23311d12822120ebf5e198f80dc504
Jose Manuel Mendez <jmanuel34@gmail.com>
2025-01-31 13:57:53 +0100

    Cliente, cuenta y tarjeta probados

---
+++ p01mdp-modelo/src/main/java/es/cursosprhib/mediosdepago/modelo/App.java
@@ -0,0 +1,21 @@
+package es.cursosprhib.mediosdepago.modelo;
+
+import jakarta.persistence.EntityManager;
+
+public class App {
+
+	public static void main(String[] args) {
+		EntityManager em = Emf.getInstance().createEntityManager();
+	/*
+		Cliente c = em.find(Cliente.class, 4);
+		System.out.println(c.apellido1);
+		Cuenta cuenta = em.find(Cuenta.class, 10);
+		System.out.println(cuenta.getNroCuenta());
+	//*/
+		//*		
+		Tarjeta t = em.find(Tarjeta.class, 10 );
+		System.out.println(t.getPan());
+//*/
+	}
+
+}
